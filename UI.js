dda54d8d3c01799b8a4f2276c1e59d0e5def5723
karlkukk <karlkukk97@gmail.com>
2020-12-01 14:09:47 +0200

    Get contact data / delete contact /

--- UI.js
+++ UI.js
@@ -2,15 +2,42 @@ class UI {
     addPersonToTable(person) {
         // create tr
         const tr = document.createElement('tr');
-
         // create all td with person data
         tr.innerHTML = `<td>${person.firstName}</td>
                         <td>${person.lastName}</td>
                         <td>${person.city}</td>
                         <td>${person.street}</td>
                         <td>${person.postcode}</td>
-                        <td>${person.phone}</td>`;
+                        <td>${person.phone}</td>
+                        <td><a href="#"><i class="fas fa-backspace"></i></a></td>
+        `;
         // append tr to table
         contacts.appendChild(tr);
-}
+    }
+
+    alertMessage(message, style) {
+        // create div for alert message
+        const div = document.createElement('div');
+        // add class to div
+        div.className = `alert ${style}`;
+        // create string value of message for div
+        const text = document.createTextNode(message);
+        // insert text into div
+        div.appendChild(text);
+        // find components - before and after alert div element
+        const card = document.querySelector('.card-content');
+        const form = document.querySelector('#contact-form');
+        // add alert info html
+        card.insertBefore(div, form);
+        // set alert messega up to 5 sec
+        setTimeout(function () {
+            document.querySelector(".alert").remove();
+        }, 5000);
+    }
+
+    deletePersonFromTable(eventElement) {
+        const personContact = eventElement.parentElement.parentElement.parentElement;
+        personContact.remove();
+    }
+
 }
--- app.js
+++ app.js
@@ -1,31 +1,90 @@
 // define variables
+
+// app data html elements
 const form = document.querySelector('#contact-form');
-const contacts = document.querySelector('#contacts-table')
-const firstName = document.querySelector('#first_name');
-const lastName = document.querySelector('#last_name');
-const city = document.querySelector('#city');
-const street = document.querySelector('#street');
-const postcode = document.querySelector('#postcode');
-const phone = document.querySelector('#phone');
-
-const contact = [firstName, lastName, city, street, postcode, phone];
-
+const contacts = document.querySelector('#contacts-table');
+const clearBtn = document.querySelector('#clear-tasks');
 
 // define event listeners
-// add contact to table
+//page reload event - get data from local storage
+document.addEventListener('DOMContentLoaded', getContacts);
+// add contact to table - submit button
 form.addEventListener('submit', addContact);
+//delete contact
+contacts.addEventListener('click', deleteContact);
+clearBtn.addEventListener('click', clearTasks);
 
-// functions
-// addContact fucntion
-
+// project functions
+// addContact
 function addContact(e) {
-    if (firstName.value === '' || lastName.value === '' || city.value === '' || street.value === '' || postcode.value === '' || phone.value === '') {
-        alert("Add new contact data!")
+    // contact data from form element
+    const firstName = document.querySelector('#first_name').value;
+    const lastName = document.querySelector('#last_name').value;
+    const city = document.querySelector('#city').value;
+    const street = document.querySelector('#street').value;
+    const postCode = document.querySelector('#postcode').value;
+    const phone = document.querySelector('#phone').value;
+
+    // create new ui object
+    const ui = new UI();
+    //create new ls object
+    const ls = new LS();
+
+    // control form data
+    if (firstName === "" | lastName === "" | city === "" | street === "" | postCode === "" | phone === "") {
+        ui.alertMessage("Add ALL new contact data!", "problem");
     } else {
-        const person = new Person(firstName.value, lastName.value, city.value, street.value, postcode.value, phone.value);
-        const ui = new UI();
+        // create new person object with form data
+        const person = new Person(firstName, lastName, city, street, postCode, phone);
+        // add person object data to html table
         ui.addPersonToTable(person);
+        //save persons data to ls
+        ls.saveContact(person);
+        ui.alertMessage("added contact to address book", "ok");
+        person.firstName.value = '';
         e.preventDefault();
     }
 }
 
+function deleteContact(e) {
+    const ui = new UI();
+    const ls = new LS();
+    const deleteBtn = e.target;
+    const firstname = (deleteBtn.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling
+        .previousElementSibling.previousElementSibling.textContent);
+    const lastname = (deleteBtn.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling
+        .previousElementSibling.textContent);
+    console.log(firstname, lastname);
+    ui.deletePersonFromTable(e.target);
+    const isDeleted = ls.deleteContact(firstname, lastname);
+    //set alert
+    if (isDeleted) {
+        ui.alertMessage("Contact was deleted!", "ok");
+    } else {
+        ui.alertMessage("Problem with deleteing data", "problem");
+    }
+    e.preventDefault();
+}
+
+function getContacts() {
+    const ls = new LS();
+    const ui = new UI();
+    const persons = ls.getContacts();
+    //get each contact and transform to Person object
+    persons.forEach(function (person) {
+        const personData = new Person(
+            person['firstName'],
+            person['lastName'],
+            person['city'],
+            person['street'],
+            person['postcode'],
+            person['phone']);
+        //create ui object for html table row
+        ui.addPersonToTable(personData);
+    })
+}
+
+function clearTasks(e) {
+    contacts.innerHTML = '';
+    localStorage.clear();
+}
